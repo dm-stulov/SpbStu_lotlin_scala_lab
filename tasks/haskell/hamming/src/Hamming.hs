e7c4c00cc4ea5352afdd3125568842bb8ee6651b
Stulov Dima <dm-stulov@yandex.ru>
2020-04-05 15:05:13 +0300

    add my lab of scala

---
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -0,0 +1,5 @@
+module Hamming (distance) where
+distance :: String -> String -> Maybe Int
+distance xs ys = if ((length xs) /= (length ys))
+                   then Nothing
+                   else Just (length (filter (uncurry (/=)) (zip xs ys)))
